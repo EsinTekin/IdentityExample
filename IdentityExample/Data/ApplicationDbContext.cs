ee3fff61c02c201083caffe97b1cc1345a0b877b
Esin Tekin <esnntekin@gmail.com>
2020-12-03 02:05:43 +0300

    Second commit
    User name - DayOfBirth - Address - Gender

--- IdentityExample/Data/ApplicationDbContext.cs
+++ IdentityExample/Data/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IdentityExample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace IdentityExample.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- IdentityExample/Data/Migrations/20201202091415_MigrationName.Designer.cs
+++ IdentityExample/Data/Migrations/20201202091415_MigrationName.Designer.cs
@@ -4,14 +4,16 @@ using IdentityExample.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityExample.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201202091415_MigrationName")]
+    partial class MigrationName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
